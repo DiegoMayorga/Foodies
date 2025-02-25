c9cd924ec19ac1cd16576365f9829c7444bca5f7
DiegoMayorga <diegoamayorgat@gmail.com>
2025-02-25 15:08:44 -0500

    Preparing the map feature to display all the meals object.

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,23 +1,27 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 export default function MealsPage() {
   return (
     <>
-      <h1 style={{ textAlign: "center", color: "white" }}>Meals page</h1>
-      <div style={{ textAlign: "center" }}>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
         <p>
-          <Link href="/community">Community</Link>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
         </p>
-        <p>
-          <Link href="/meals/share">Share Meal</Link>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Share Your Favorite Recipe
+          </Link>
         </p>
-        <p>
-          <Link href="/meals/item-1">Item-1</Link>
-        </p>
-        <p>
-          <Link href="../">Back</Link>
-        </p>
-      </div>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 }
